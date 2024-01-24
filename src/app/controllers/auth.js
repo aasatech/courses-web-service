02c399e961dabbad0f5ee6053af1f0d8c705e748
Chhayuth <chhayuth0@gmail.com>
2024-01-24 10:10:10 +0700

    s

--- src/app/controllers/auth.js
+++ src/app/controllers/auth.js
@@ -13,13 +13,6 @@ export const register = async (req, res) => {
       return res.status(400).json({ errors: _.groupBy(result.array(), 'path') })
     }
 
-    const existingUser = await User.query().findOne({ email })
-
-    //
-    if (existingUser) {
-      return res.status(400).json({ message: 'Email already exist' })
-    }
-
     const encryptedPassword = User.generatePassword(password)
 
     const user = await User.query().insert({
